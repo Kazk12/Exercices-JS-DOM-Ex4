9fc95c866ea0ac11ee93a2bd64e2d7c71542f0c2
Kazk12 <kazuko43KO@gmail.com>
2024-11-20 15:32:08 +0100

    DONE : Exo5

--- Ex5_Carrousel/main.js
+++ Ex5_Carrousel/main.js
@@ -3,22 +3,58 @@ const Gauche = document.querySelector("#divLeft");
 const Droite = document.querySelector("#divRight");
 
 const images = {
-    1 : "./images/1.png";
-    2 : "./images/2.png";
-    3 : "./images/3.png";
-}
+    "1" : "./images/1.jpg",
+    "2" : "./images/2.jpg",
+    "3" : "./images/3.jpg",
+};
 
-console.log(images);
+
+let tableau = Object.entries(images);
+
+let currentIndex = 0;  // L'index de l'image actuelle, initialisé à 0 pour la première image
+
+let img = document.querySelector(".imgCarousel");
+
+// Initialisation de l'image visible
+
+
+img.src = tableau[currentIndex][1];  // Affiche la première image au chargement
+
+
 
 
 bouton.addEventListener("click", handleClickShowImage);
 Gauche.addEventListener("click", handleClickChangeImageGauche);
-// Droite.addEventListener("click", handleClickChangeImageDroite);
+Droite.addEventListener("click", handleClickChangeImageDroite);
 
-function handleClickShowImage(event){
-    document.querySelector(".imgCarousel").style.visibility = "visible";
+function handleClickShowImage(){
+    img.style.visibility = "visible";
 }
 
-function handleClickChangeImageGauche(event){
+
+
+function handleClickChangeImageGauche(){
+    if(currentIndex === 0){
+        currentIndex = tableau.length - 1;
+    } else {
+        currentIndex -= 1;
+    }
+    img.src = tableau[currentIndex][1];
     
-}
+ }
+
+function handleClickChangeImageDroite(){
+     if(currentIndex === tableau.length){
+            currentIndex = 0;
+     } else {
+            currentIndex += 1;
+     }
+    img.src = tableau[currentIndex][1];
+        
+ }
+    
+
+
+
+
+
